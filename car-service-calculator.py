b8347dc37db00c9a00a92582e9da67ed6850d06b
brandon <fordyb10@gmail.com>
2026-02-23 14:35:12 +0000

    refactor code into funtions

--- car-service-calculator.py
+++ car-service-calculator.py
@@ -1,16 +1,26 @@
-car_mileage = int(input("Enter current car mileage: "))
-last_service = int(input("Enter mileage at last service: "))
+def calculate_service (car_mileage, last_service):
+    miles_since_service = car_mileage - last_service
+    miles_until_next_service = 10000 - miles_since_service
+    return miles_since_service, miles_until_next_service
 
-miles_since_service = car_mileage - last_service 
-miles_until_next_service = 10000 - miles_since_service
-amiles_until_next_service = miles_until_next_service*-1
+def display_message (miles_until_next_service):
+    if miles_until_next_service <= 0:
+        print (f"Your car is over due for a service by {-miles_until_next_service} miles")
+    elif miles_until_next_service <=1000:
+        print (f"Your car is due for a service in {miles_until_next_service} miles")
+    else:
+        print (f"you have {miles_until_next_service} miles until your next service")
 
-print ("Miles driven since last service:" , miles_since_service)
-print ("miles until next service:" , miles_until_next_service)
+def main():
+    car_mileage = int(input("Enter your current cars mileage:"))
+    last_service = int(input("Enter your cars mileage at last service:"))
+    
+    miles_since_service, miles_until_next_service = calculate_service(car_mileage, last_service)
 
-if miles_until_next_service <= 0:
-    print (f"Your car is {amiles_until_next_service} overdue for a service!")
-elif miles_until_next_service <= 1000:
-    print (f"Your service is due soon. only {miles_until_next_service} remaining")
-else:
-    print (f"There is no service due yet. You have {miles_until_next_service} remaining")
+    print ("miles driven since last service:", miles_since_service)
+    print ("miles until next service:", miles_until_next_service)
+
+    display_message (miles_until_next_service)
+
+
+main()
