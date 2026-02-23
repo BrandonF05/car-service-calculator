f12ce6bb7d600f9c5169bd656ee43c4b79f978ea
brandon <fordyb10@gmail.com>
2026-02-23 14:14:41 +0000

    Add service due and overdue warning logic

--- car-service-calculator.py
+++ car-service-calculator.py
@@ -3,13 +3,14 @@ last_service = int(input("Enter mileage at last service: "))
 
 miles_since_service = car_mileage - last_service 
 miles_until_next_service = 10000 - miles_since_service
+amiles_until_next_service = miles_until_next_service*-1
 
 print ("Miles driven since last service:" , miles_since_service)
 print ("miles until next service:" , miles_until_next_service)
 
 if miles_until_next_service <= 0:
-    print ("Your car is overdue for a service!")
+    print (f"Your car is {amiles_until_next_service} overdue for a service!")
 elif miles_until_next_service <= 1000:
-    print ("Your service is due soon")
+    print (f"Your service is due soon. only {miles_until_next_service} remaining")
 else:
-    print ("There is no service due yet.")
+    print (f"There is no service due yet. You have {miles_until_next_service} remaining")
